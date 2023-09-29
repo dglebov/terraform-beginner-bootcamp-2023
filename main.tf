90c16247be5ac7255ac3a8ab66af662c22f057d5
Dmitriy Glebov <glebovds@gmail.com>
2023-09-29 17:01:48 +0000

    #19 change structure of root modules

--- main.tf
+++ main.tf
@@ -1,36 +1,3 @@
-terraform {
-  #backend "remote" {
-  #  hostname = "app.terraform.io"
-  #  organization = "dglebov_org"#
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
-  cloud {
-    organization = "dglebov_org"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   length           = 32
   lower            = true
@@ -42,11 +9,6 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+    user_uuid        = var.user_uuid
+   }
 }
