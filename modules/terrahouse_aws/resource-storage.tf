a87bcc8b035c2e54d6f8c85451eef7af6256655d
Dmitriy <dglebov@users.noreply.github.com>
2023-10-12 11:11:14 +0100

    #29 only upload index.html if content version changed (#30)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -25,6 +25,11 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
   content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+    
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -33,6 +38,11 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
   content_type = "text/html"
   etag = filemd5(var.error_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+    
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -56,4 +66,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
